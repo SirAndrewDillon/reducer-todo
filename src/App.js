116605e91f2e647be6438012d698591dbcc9ac41
andrew dillon <majik895@gmail.com>
2019-07-23 16:43:36 -0400

    Adding Reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import './App.css'
+import TodoApp from './TodoApp'
 
 function App() {
-	return (
-		<div className="App">
-			<h1>App Component</h1>
-		</div>
-	)
+	return <TodoApp />
 }
 
 export default App
